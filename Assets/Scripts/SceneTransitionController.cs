8c118911b7dc39111fb8ffdd78c16bd9c722ee34
Drew <dmbushnell@hotmail.com>
2021-07-17 21:03:18 -0700

    Added a bit more to SceneTransitionController

--- Assets/Scripts/SceneTransitionController.cs
+++ Assets/Scripts/SceneTransitionController.cs
@@ -2,34 +2,103 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using System;
 
 public class SceneTransitionController : MonoBehaviour
 {
-    public GameObject StartScene;
-    public GameObject EndScene;
-
-    public UnityEvent StartSceneTransition;
-    public UnityEvent EndSceneTransition;
-    public UnityEvent BeforeSceneUnload;
-    public UnityEvent AfterSceneLoad;
-
-    public enum GAMES {
-        HOOK
+    private static SceneTransitionController instance;
+    public static SceneTransitionController Instance {
+        get {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<SceneTransitionController>();
+            }
+            return instance;
+        }
     }
 
-    // Start is called before the first frame update
-    void Start()
+    public event Action StartGameTransition = delegate { };
+    public event Action EndGameTransition = delegate { };
+    public event Action OnGameUnload = delegate { };
+    public event Action OnGameLoad = delegate { };
+
+    protected CanvasGroup faderCanvasGroup;
+    protected float fadeDuration = 1f;
+
+    protected GAMES currGame;
+    public char spawnChar = '_';
+
+    private bool isFading;
+    public bool IsFading { get { return isFading; } }
+
+    public MetaGameManager mgm;
+
+    private void Awake()
     {
-        
+        if (instance != null && instance != this) { Destroy(this); }
+        else { instance = this; }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void FadeAndLoadGame(GAMES _loadGame, char _spawnChar)
     {
-        
+        if (isFading) { return; }
+
+        StartCoroutine(FadeAndSwitchScenes(_loadGame, _spawnChar));
     }
 
-    void FadeAndLoadScene(GAMES game, GameObject spawnPoint) {
+    // This is the main coroutine where it goes through the entire game transition
+    private IEnumerator FadeAndSwitchScenes(GAMES _loadGame, char _spawnChar)
+    {
+        //!TODO: Tell game manager that player can't input here
 
+        yield return StartCoroutine(Fade(1f));
+        faderCanvasGroup.alpha = 1f;
+        OnGameUnload?.Invoke();
+
+        yield return null;
+
+        currGame = _loadGame;
+        spawnChar = _spawnChar;
+        OnGameLoad?.Invoke();
+
+        yield return StartCoroutine(Fade(0f));
+
+        //!TODO: Tell game manager that player can input here
     }
+
+    //Coroutine to fade the loading screen in and out
+    public IEnumerator Fade(float finalAlpha)
+    {
+        isFading = true;
+
+        if (finalAlpha == 1f)
+        {
+            StartGameTransition?.Invoke();
+        }
+
+        faderCanvasGroup.blocksRaycasts = true;
+
+        float fadeSpeed = Mathf.Abs(faderCanvasGroup.alpha - finalAlpha) / fadeDuration;
+
+        while (!Mathf.Approximately(faderCanvasGroup.alpha, finalAlpha))
+        {
+            faderCanvasGroup.alpha = Mathf.MoveTowards(faderCanvasGroup.alpha, finalAlpha, fadeSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        isFading = false;
+
+        if (finalAlpha == 0f)
+        {
+            EndGameTransition?.Invoke();
+        }
+
+        faderCanvasGroup.blocksRaycasts = false;
+    }
+}
+
+public enum GAMES
+{
+    NONE,
+    HOOK
 }
