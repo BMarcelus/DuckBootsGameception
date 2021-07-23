b0211f6b52594683728b4cd0b32627de4bbe873c
Patrickgh3 <patrickjtraynor@gmail.com>
2021-07-23 10:56:55 -0700

    Add WarpPoint prefab with animation

--- Assets/Scripts/WarpPoint.cs
+++ Assets/Scripts/WarpPoint.cs
@@ -5,18 +5,49 @@ using UnityEngine;
 public class WarpPoint : MonoBehaviour
 {
     public GameManager game;
+
+    // Used https://onlinegiftools.com/convert-gif-to-sprite-sheet to convert from gif to sprite sheet
+    public Sprite[] introFrames;
+    public Sprite[] idleFrames;
+    public Sprite[] hoverFrames;
+    const float animSpeed = 5f;
+    float t;
+    enum State {
+        Opening,
+        Idle,
+        Hover,
+    }
+    State state;
+    SpriteRenderer sr;
+
     private void OnTriggerEnter2D(Collider2D other) {
+        state = State.Hover;
+        t = 0;
         MetaGameManager.instance.SwitchToGame(game);
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        sr = GetComponent<SpriteRenderer>();
     }
+    void Update() {
+        // Check to see if we have set sprites, so we don't break people's scenes that don't have sprites set
+        if (introFrames.Length == 0)  return;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Progress through animations
+        if (state == State.Opening) {
+            sr.sprite = introFrames[(int)Mathf.Floor(t)];
+            t += Time.deltaTime*animSpeed;
+            if (t >= introFrames.Length) {
+                state = State.Idle;
+                t = 1; // Set t to 1 instead of 0 since there is a duplicated frame in this transition
+            }
+        }
+        else if (state == State.Idle) {
+            t += Time.deltaTime*animSpeed;
+            sr.sprite = idleFrames[(int)Mathf.Floor(t) % idleFrames.Length];
+        }
+        else if (state == State.Hover) {
+            t += Time.deltaTime*animSpeed;
+            sr.sprite = hoverFrames[(int)Mathf.Floor(t) % hoverFrames.Length];
+        }
     }
 }
