e2d93eaa72f6c583f7f96b6abf4756d37767c702
Drew <dmbushnell@hotmail.com>
2021-08-01 12:53:48 -0700

    Added structure for soundbank

---
+++ Assets/Scripts/SoundBank.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundBank : MonoBehaviour
+{
+    [SerializeField]
+    private SoundInfo[] sInfo;
+
+    private Dictionary<SoundType, AudioClip> sDict = new Dictionary<SoundType, AudioClip>();
+
+    private static SoundBank instance;
+    public static SoundBank Instance
+    {
+        get {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<SoundBank>();
+            }
+            return instance;
+        }
+    }
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this) { Destroy(this); }
+        else { instance = this; }
+
+        foreach(SoundInfo si in sInfo)
+        {
+            if (sDict.ContainsKey(si.key))
+            {
+                Debug.LogError("The key " + si.key + " is repeated in the sound bank!");
+                continue;
+            }
+
+            sDict.Add(si.key, si.sound);
+        }
+
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlaySound(SoundType sType)
+    {
+        if (!sDict.ContainsKey(sType)) { return; }
+
+        audioSource.PlayOneShot(sDict[sType]);
+    }
+}
+
+[System.Serializable]
+public struct SoundInfo
+{
+    public AudioClip sound;
+    public SoundType key;
+}
+
+public enum SoundType
+{
+    None,
+
+}
